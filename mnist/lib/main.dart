8df8c25eef1e4766548c9125eda965095166be75
abhinav-TB <abhinavtb@gmail.com>
2021-05-07 18:21:20 +0530

    main ui done

---
+++ mnist/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:mnist/pages/upload.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Home());
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+int currentIndex = 0;
+List tabs = [UploadImage(), Center(child: Text("Drawing page"))];
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: tabs[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        selectedFontSize: 14.0,
+        unselectedFontSize: 14.0,
+        selectedItemColor: Colors.pink,
+        unselectedItemColor: Colors.grey[400],
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.image), label: "Image"),
+          BottomNavigationBarItem(icon: Icon(Icons.album), label: "Draw")
+        ],
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
